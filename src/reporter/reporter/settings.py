f29bd6cd9102d7a9e59663a588894971268efb54
hew <mrhewschool@gmail.com>
2019-09-28 12:34:28 +0800

    mysql connection

--- src/reporter/reporter/settings.py
+++ src/reporter/reporter/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'reporter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'CitiHack',
+        'USER': 'admin',
+        'PASSWORD':'citihack193',
+        'HOST':'db-team3.cif14ai3iiw6.ap-southeast-1.rds.amazonaws.com',
+        'PORT':'3306',
     }
 }
 
