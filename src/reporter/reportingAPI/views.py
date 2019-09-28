8de8a4c4d3c9454a19a0deb722cf820b3a7a4364
brendonyeo <44076587+brendonyeo@users.noreply.github.com>
2019-09-28 15:28:26 +0800

    Demographics WIP

--- src/reporter/reportingAPI/views.py
+++ src/reporter/reportingAPI/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
+from .models import Event, User, Eventcategory
+from django.http import JsonResponse
+import json
 
 # Create your views here.
 def demographic(request):
     eventid = request.GET.get('eventId', None)
+    eventdetails = Event.objects.get(id__exact=eventid)
+    eventcategory = Eventcategory.objects.filter(eventid=eventid)
+    print(eventcategory)
     if eventid == None:
         pass #Return error
-    print(eventId)
 
-    return render(request, 'index.html')
+    data = {
+        "eventId": eventdetails.id,
+        "eventName":eventdetails.eventname,
+        "startDateTime":eventdetails.startdatetime,
+        "endDateTime":eventdetails.enddatetime,
+        "numParticipants":eventdetails.maxparticipants,
+        "organizerName":eventdetails.organizername,
+        # "categoryId":,
+        # "volunteers":,
+        "eventStatus": eventdetails.eventstatus
+    }
+    return JsonResponse(data)
 
 def organization(request):
     organizerName = request.GET.get('organizerName', None)
