35e92578544967cd9557b2815e01d8ee653e47a5
brendonyeo <44076587+brendonyeo@users.noreply.github.com>
2019-09-28 16:04:11 +0800

    Demographic response complete
    Need refactor value to the exact output style

--- src/reporter/reportingAPI/views.py
+++ src/reporter/reportingAPI/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Event, User, Eventcategory
+from .models import Event, User, Eventcategory, Eventregistration
 from django.http import JsonResponse
 import json
 
@@ -7,8 +7,17 @@ import json
 def demographic(request):
     eventid = request.GET.get('eventId', None)
     eventdetails = Event.objects.get(id__exact=eventid)
-    eventcategory = Eventcategory.objects.filter(eventid=eventid)
-    print(eventcategory)
+    eventcategories = Eventcategory.objects.values_list('categoryid', flat=True).filter(eventid=eventid)
+    eventvolunteersid = Eventregistration.objects.values_list('userid', flat=True).filter(eventid=eventid)
+
+    volunteers = []
+    for value in eventvolunteersid:
+        user = User.objects.get(id__exact=value).__dict__
+        del user['_state']
+        volunteers.append(user)
+
+    categories = [value for value in eventcategories]
+
     if eventid == None:
         pass #Return error
 
@@ -19,8 +28,8 @@ def demographic(request):
         "endDateTime":eventdetails.enddatetime,
         "numParticipants":eventdetails.maxparticipants,
         "organizerName":eventdetails.organizername,
-        # "categoryId":,
-        # "volunteers":,
+        "categoryId":categories,
+        "volunteers":volunteers,
         "eventStatus": eventdetails.eventstatus
     }
     return JsonResponse(data)
