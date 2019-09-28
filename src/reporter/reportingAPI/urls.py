8124621e18359fee740b0bddf2105901def87f0c
brendonyeo <44076587+brendonyeo@users.noreply.github.com>
2019-09-28 14:09:44 +0800

    Created endpoint

--- src/reporter/reportingAPI/urls.py
+++ src/reporter/reportingAPI/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('demographic/', views.index, name='index')
+    path('demographic', views.demographic, name='demographic'),
+    path('organization', views.organization, name='organization'),
+    path('historical', views.historical, name='historical'),
+    path('user-historical', views.user_historical, name='user_historical'),
 ]
--- src/reporter/reportingAPI/views.py
+++ src/reporter/reportingAPI/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    a = request.GET.get('eventId', 'none')
-    print(a)
+def demographic(request):
+    eventid = request.GET.get('eventId', None)
+    if eventid == None:
+        pass #Return error
+    print(eventId)
+
     return render(request, 'index.html')
+
+def organization(request):
+    organizerName = request.GET.get('organizerName', None)
+    pass
+
+def historical(request):
+    fromdate = request.GET.get('fromDate', None)
+    todate = request.GET.get('toDate', None)
+    pass
+
+def user_historical(request):
+    userid = request.GET.get('userId', None)
+    fromdate = request.GET.get('fromDate', None)
+    todate = request.GET.get('toDate', None)
+    pass
