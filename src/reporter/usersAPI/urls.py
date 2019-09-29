72c4dcc89e969d6379594a6ebf73aefc496df603
joshua <>
2019-09-29 12:02:28 +0800

    done with reset-password

--- src/reporter/usersAPI/urls.py
+++ src/reporter/usersAPI/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.accounts_details, name ='accounts_details'),
-    # path('reset-password/',views.reset_password, name ='reset-password'),
+    path('reset-password/',views.reset_password, name ='reset-password'),
     # path('account-type/',views.account_type, name='account_type'),
 
     # path('', views.UserList.as_view(),name='user-list'),
